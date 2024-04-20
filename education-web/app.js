226308313fc8ca533e4780aa19da865450fc3e29
Azizu Haque <azizul.contact.me@gmail.com>
2024-04-21 01:25:49 +0300

    done with FAQ section

--- education-web/app.js
+++ education-web/app.js
@@ -3,3 +3,18 @@ window.addEventListener("scroll", function () {
     .querySelector("nav")
     .classList.toggle("scroll-nav", window.scrollY > 0);
 });
+
+// faqs section
+const questions = document.querySelectorAll(".question");
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+  console.log(btn);
+  btn.addEventListener("click", function () {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+    question.classList.toggle("show-text");
+  });
+});
